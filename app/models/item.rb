cdeb1dcb7ab97e49a6d184895e0ddd3981a54c59
Aras Pranckevicius <aras@unity3d.com>
2014-02-23 20:42:55 +0200

    bootstrap form

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ActiveRecord::Base
 	mount_uploader :picture, PictureUploader
 	attr_accessible :bloom1, :bloom2, :color, :conditions, :height, :latin, :name, :picture, :width
+	validates :name, :presence => true
 end
