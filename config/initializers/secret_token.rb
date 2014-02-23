c506e8b09bdcd227495aae48432db39f7c94b3aa
Aras Pranckevicius <aras@unity3d.com>
2014-02-23 18:28:50 +0200

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cataloq::Application.config.secret_token = 'adf7237edef30729e6b2b1270f5cf17a71db6bd9bd51deb8879c76f9701bb7824280161593eb63cc73ea5621fefa7894f85c121ccc4de972a8728b647da8c112'
