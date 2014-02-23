383b9a2685627c8acc621815d397c51af7fa0f04
Aras Pranckevicius <aras@unity3d.com>
2014-02-23 19:54:46 +0200

    picture uploads

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ActiveRecord::Base
-  attr_accessible :bloom1, :bloom2, :color, :conditions, :height, :latin, :name, :picture, :width
+	mount_uploader :picture, PictureUploader
+	attr_accessible :bloom1, :bloom2, :color, :conditions, :height, :latin, :name, :picture, :width
 end
