a90f16706c4836c7756d5bd899b5feadd79651b1
Aras Pranckevicius <aras@unity3d.com>
2014-02-24 09:00:16 +0200

    tweaking UI

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,9 @@ class ItemsController < ApplicationController
   def self.months()
     Array['Visi', 'Sausis', 'Vasaris', 'Kovas', 'Balandis', 'Geguze', 'Birzelis', 'Liepa', 'Rugpjutis', 'Rugsejis', 'Spalis', 'Lapkritis', 'Gruodis']
   end
+  def self.monthOptions()
+    ItemsController.months().drop(1).each_with_index.map { |x,i| [x,i+1] }
+  end
 
   # GET /items
   # GET /items.json
